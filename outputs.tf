481ed526c50303318404a6103eeaecfa7981f1fa
Daniel Allen <dan@aeternagroup.net>
2026-01-11 14:42:41 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "data_protection_resource_guards" {
+  description = "All data_protection_resource_guard resources"
+  value       = azurerm_data_protection_resource_guard.data_protection_resource_guards
+}
+
