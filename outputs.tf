d718c55b2a86110a516caf4d18ccaddf7a98e591
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:01:45 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "data_protection_resource_guards_id" {
+  description = "Map of id values across all data_protection_resource_guards, keyed the same as var.data_protection_resource_guards"
+  value       = { for k, v in azurerm_data_protection_resource_guard.data_protection_resource_guards : k => v.id }
+}
 output "data_protection_resource_guards_location" {
   description = "Map of location values across all data_protection_resource_guards, keyed the same as var.data_protection_resource_guards"
   value       = { for k, v in azurerm_data_protection_resource_guard.data_protection_resource_guards : k => v.location }
